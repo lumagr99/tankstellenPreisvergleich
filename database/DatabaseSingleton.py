812135bea484f36a30035a9b27e64819ae327c68
lumagr99 <lumagr.mail@gmail.com>
2021-01-21 17:41:57 +0100

    DatabaseSingleton.py ausgelagert.

---
+++ database/DatabaseSingleton.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+
+def __init__():
+    print("hello")
+
+
+class DatabaseSingleton:
+    __instance__ = None
+    __connection__ = mysql.connector.connect(
+        host="192.168.178.54",
+        user="tankstellenData",
+        password="tankstellenData2021",
+        database="tankstellenData"
+    )
+
+    def __init__(self):
+        if DatabaseSingleton.__instance__ is None:
+            DatabaseSingleton.__instance__ = self
+        else:
+            raise Exception("Du kannst keine weitere Instanz erzeugen!")
+
+    @staticmethod
+    def getInstance():
+        if not DatabaseSingleton.__instance__:
+            DatabaseSingleton()
+        return DatabaseSingleton.__instance__
+
+    def getCursor(self):
+        return self.__connection__.cursor()
--- service.py
+++ service.py
@@ -1,39 +1,15 @@
-from flask import Flask
-from flask import request
-import mysql.connector
 import json
 from datetime import datetime
+
+from flask import Flask
+from flask import request
+
+from database.DatabaseSingleton import DatabaseSingleton
 
 app = Flask(__name__)
 
 
 # TODO fehlerhafte parameter abfangen
-
-
-class DatabaseSingleton:
-    __instance__ = None
-    __connection__ = mysql.connector.connect(
-        host="192.168.178.54",
-        user="tankstellenData",
-        password="tankstellenData2021",
-        database="tankstellenData"
-    )
-
-    def __init__(self):
-        if DatabaseSingleton.__instance__ is None:
-            DatabaseSingleton.__instance__ = self
-        else:
-            raise Exception("Du kannst keine weitere Instanz erzeugen!")
-
-    @staticmethod
-    def getInstance():
-        if not DatabaseSingleton.__instance__:
-            DatabaseSingleton()
-        return DatabaseSingleton.__instance__
-
-    def getCursor(self):
-        return self.__connection__.cursor()
-
 
 d = DatabaseSingleton()
 
