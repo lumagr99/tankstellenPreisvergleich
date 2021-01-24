c2cd13ad5f16b1b47ec473a2157e2fe094bdc7ea
lumagr99 <lumagr.mail@gmail.com>
2021-01-24 16:47:42 +0100

    Zentrale startWebapplication.py zum Starten der Webanwendung hinzugefügt.

--- backend/database/DatabaseSingleton.py
+++ backend/database/DatabaseSingleton.py
@@ -3,16 +3,17 @@ import mysql.connector
 
 class DatabaseSingleton:
     __instance__ = None
-    __connection__ = mysql.connector.connect(
-        host="192.168.178.54",
-        user="tankstellenData",
-        password="tankstellenData2021",
-        database="tankstellenData"
-    )
+    __connection__ = ""
 
-    def __init__(self):
+    def __init__(self, dataURL):
         if DatabaseSingleton.__instance__ is None:
             DatabaseSingleton.__instance__ = self
+            self.__connection__ = mysql.connector.connect(
+                host=dataURL,
+                user="tankstellenData",
+                password="tankstellenData2021",
+                database="tankstellenData"
+            )
         else:
             raise Exception("Du kannst keine weitere Instanz erzeugen!")
 
---
+++ startWebapplication.py
@@ -0,0 +1,8 @@
+from backend.service import service
+
+service = service("192.168.178.54")
+
+running = True
+
+while running:
+    pass
