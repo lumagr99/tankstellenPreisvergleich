dead4dd6167a0a80054d2c7dfad9ebb1d66ee529
lumagr99 <lumagr.mail@gmail.com>
2021-01-27 20:37:21 +0100

    Datenbank aktualisiert.

--- backend/database/DatabaseSingleton.py
+++ backend/database/DatabaseSingleton.py
@@ -5,14 +5,14 @@ class DatabaseSingleton:
     __instance__ = None
     __connection__ = ""
 
-    def __init__(self, dataURL):
+    def __init__(self):
         if DatabaseSingleton.__instance__ is None:
             DatabaseSingleton.__instance__ = self
             self.__connection__ = mysql.connector.connect(
-                host=dataURL,
-                user="tankstellenData",
-                password="tankstellenData2021",
-                database="tankstellenData"
+                host="45.88.109.79",
+                user="tankstellenCrawler",
+                password="qGD0zc5iKsvhyjwO",
+                database="tankdaten"
             )
         else:
             raise Exception("Du kannst keine weitere Instanz erzeugen!")
