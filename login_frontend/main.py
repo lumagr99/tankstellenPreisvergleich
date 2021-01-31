98993d589bb8cefa3e14f88bd3a594f3278d16af
lumagr99 <lumagr.mail@gmail.com>
2021-01-31 17:52:16 +0100

    Registration hinzugefügt.

--- login_frontend/main.py
+++ login_frontend/main.py
@@ -19,10 +19,10 @@ mysql = MySQL(app)
 def login():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        username = request.form['username']
+        benutzername = request.form['username']
         password = request.form['password']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM Benutzer WHERE benutzername = %s AND password = %s', (username, password))
+        cursor.execute('SELECT * FROM Benutzer WHERE benutzername = %s AND password = %s', (benutzername, password))
         account = cursor.fetchone()
         if account:
             session['loggedin'] = True
@@ -33,10 +33,48 @@ def login():
             msg = 'Ups, das war wohl nichts!'
     return render_template('index.html', msg=msg)
 
+
 def logout():
     session.pop('loggedin', None)
     session.pop('id', None)
     session.pop('benutzername', None)
     return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = ''
+
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'password' in request.form:
+        benutzername = request.form['username']
+        password = request.form['password']
+        msg = createAccount(benutzername, password)
+    elif request.method == 'POST':
+        msg = 'Füll bitte das Formular aus!'
+
+    return render_template('register.html', msg=msg)
+
+
+"""Validiert ob ein Account existiert, wenn nicht erstellt es einen.
+Gibt eine passende Fehlermeldung zurück."""
+
+
+def createAccount(benutzername, password):
+    msg = ''
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM Benutzer WHERE benutzername = %s', (benutzername,))
+    account = cursor.fetchone()
+
+    if account:
+        msg = 'Die Daten gibts leider schon!'
+    elif not benutzername or not password:
+        msg = 'Füll bitte das Formular aus!'
+    else:
+        # Account existiert nicht
+        cursor.execute('INSERT INTO Benutzer(benutzername, password) VALUES (%s, %s)', (benutzername, password,))
+        mysql.connection.commit()
+        msg = 'Du wurdest erfolgreich Registriert!'
+    return msg
+
 
 app.run()
