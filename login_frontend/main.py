74407a10a56b15921956aa5d2ec265e6a91e1a9b
lumagr99 <lumagr.mail@gmail.com>
2021-01-31 17:18:00 +0100

    Rudimäre funktion des Logins.

--- login_frontend/main.py
+++ login_frontend/main.py
@@ -6,15 +6,32 @@ import re
 app = Flask(__name__)
 app.secret_key = 'FlaskLoginTest'
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'pythonlogin'
+app.config['MYSQL_HOST'] = '45.88.109.79'
+app.config['MYSQL_USER'] = 'tankstellenCrawler'
+app.config['MYSQL_PASSWORD'] = 'qGD0zc5iKsvhyjwO'
+app.config['MYSQL_DB'] = 'tankdaten'
 
 mysql = MySQL(app)
 
 
+# TODO SALT und Hash
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    msg = 'Login erfolgreich!'
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM Benutzer WHERE benutzername = %s AND password = %s', (username, password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['benutzername'] = account['benutzername']
+            return 'Du hast dich erfolgreich eingeloggt!'
+        else:
+            msg = 'Ups, das war wohl nichts!'
     return render_template('index.html', msg=msg)
+
+
+app.run()
