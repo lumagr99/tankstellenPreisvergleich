736d162113ae2fe7d1d9dda479f1108ad817c143
lumagr99 <lumagr.mail@gmail.com>
2021-02-11 17:13:22 +0100

    Format crawler.py

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,10 +1,13 @@
-import urllib.request, json
+import urllib.request
+import json
 import mysql.connector
 
+# Abfrage der externen API.
 url = "https://creativecommons.tankerkoenig.de/json/list.php?lat=51.324&lng=7.697&rad=25&sort=dist&type=all&apikey=33e99509-7b2b-c804-469d-45a316652ef6"
 response = urllib.request.urlopen(url)
 data = json.loads(response.read())
 
+# Überprüfen ob der Anbieter die Daten für in Ordnung hält.
 if not data.get('ok'):
     raise SystemExit('error while reeding data!')
 
@@ -15,17 +18,21 @@ connection = mysql.connector.connect(
     database="tankdaten"
 )
 
-
 """Überprüft ob eine Station existiert."""
-def stationExist(stationID, cursor):
-    cursor.execute("select id from Tankstellen where id='" + stationID + "';")
+
+
+def stationExist(stationid, cursor):
+    cursor.execute("SELECT id FROM Tankstellen WHERE id='" + stationid + "';")
     if len(cursor.fetchall()) == 1:
         return True
     return False
 
+
 """Erstellt eine neue Station."""
+
+
 def createStation(station, cursor):
-    cursor.execute("insert into Tankstellen (id, name, brand, houseNumber, postCode, place, lat, lng, street)" +
+    cursor.execute("INSERT INTO Tankstellen (id, name, brand, houseNumber, postCode, place, lat, lng, street)" +
                    "VALUES (" +
                    "'" + station["id"] +
                    "', '" + station["name"] +
@@ -39,9 +46,12 @@ def createStation(station, cursor):
 
 
 """Generiert die Query um Preisdaten der Datenbank hinzuzufügen."""
+
+
 def getinsertquery(station):
-    return ("insert into Preise (id, diesel, e5, e10)" +
-            " values (" +
+    # Überprüft in der Funktion ob die Preise existieren
+    return ("INSERT INTO Preise (id, diesel, e5, e10)" +
+            " VALUE (" +
             "'" + station["id"] +
             "', '" + str([station["diesel"], '0'][station["diesel"] is None]) +
             "', '" + str([station["e5"], '0'][station["e5"] is None]) +
@@ -50,11 +60,13 @@ def getinsertquery(station):
 
 
 """Startet einen Crawl durchlauf."""
+
+
 def start():
     cursor = connection.cursor()
     try:
         for station in data.get('stations'):
-            if not stationExist(stationID=station["id"], cursor=cursor):
+            if not stationExist(stationid=station["id"], cursor=cursor):
                 createStation(station, cursor)
             cursor.execute(getinsertquery(station))
         connection.commit()
@@ -65,13 +77,3 @@ def start():
 
 # Für Cronjob start manuell auslösen.
 start()
-
-# python 3
-# mit modulen
-# import urllib.request, json
-# import mysql.connector
-# import time
-
-# sql benutzer mit passenden lesen, schreibe berechtigungen
-
-# das skript muss nach jedem system neustart einmalig neu gestartet werden
